ee3df22496d100eaeff657ba52fb7db093d6ed2e
JoelcioFacanha <joelciomoreira@gmail.com>
2020-05-20 11:10:41 -0300

    Switching player each turn

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 
 			try {
 				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces());
+				UI.printMatch(chessMatch);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -30,7 +30,7 @@ public class Program {
 
 				System.out.println();
 				System.out.print("Target: ");
-				
+
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
